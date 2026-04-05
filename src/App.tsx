e380e6b6bcaa7e2d5f76991d042c799931cd5764
hawaricracker <63854180+hawaricracker@users.noreply.github.com>
2026-04-05 20:06:17 +0700

    test_animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,120 +1,84 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [utuh, setUtuh] = useState(false)
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <div className={`flex flex-col items-center h-screen w-full ${utuh ? 'flower-utuh' : ''}`}>
+      <div className="flex w-[100%] h-[15%] justify-center items-center gap-4">
+        {!utuh && (
+          <button
+            onClick={() => setUtuh(!utuh)}
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            Mulai!!!
+          </button>
+        )}
+      </div>
+    <div className="flex justify-center items-end h-[85%] w-[100%]">
+      <svg viewBox="0 0 518 796" className="w-[44%] z-0 h-auto absolute left-[-7%]" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="Tulip">
+      <rect id="Tangkai" x="232.119" y="314" width="54" height="482" fill="#3D7B19"/>
+      <g id="Kelopak">
+      <path id="Kelopak_kiri" d="M90.9436 48.4859C163.22 52.8358 231.581 93.5915 268.063 162.204C304.545 230.816 300.105 310.28 263.295 372.632C191.019 368.282 122.658 327.527 86.176 258.915C49.6942 190.303 54.1336 110.838 90.9436 48.4859Z" fill="#CE4848"/>
+      <path id="Kelopak_kanan" d="M426.415 48.3556C354.139 52.7055 285.777 93.4612 249.295 162.074C212.813 230.686 217.253 310.15 254.063 372.502C326.339 368.152 394.7 327.397 431.182 258.785C467.664 190.172 463.225 110.708 426.415 48.3556Z" fill="#E36464"/>
+      <path id="Kelopak_tengah" d="M259.119 5C320.893 42.7723 362.119 110.851 362.119 188.56C362.119 266.268 320.893 334.346 259.119 372.118C197.346 334.346 156.119 266.268 156.119 188.56C156.119 110.851 197.346 42.7723 259.119 5Z" fill="#C76767"/>
+      </g>
+      <path id="Daun_1" d="M342.825 434.558C346.054 454.918 338.644 477.426 321.306 493.594C303.968 509.762 280.999 515.583 260.914 510.942C257.685 490.582 265.094 468.075 282.432 451.907C299.771 435.739 322.74 429.917 342.825 434.558Z" fill="#0C4C07"/>
+      <path id="Daun_0" d="M170.606 556.011C168.09 576.471 176.28 598.706 194.172 614.259C212.064 629.813 235.222 634.828 255.133 629.489C257.649 609.029 249.46 586.794 231.568 571.241C213.676 555.688 190.517 550.671 170.606 556.011Z" fill="#0C4C07"/>
+      </g>
+      </svg>
+      <svg viewBox="0 0 518 796" className="w-[35%] z-1 h-auto absolute left-[15%]" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="Tulip">
+      <rect id="Tangkai" x="232.119" y="314" width="54" height="482" fill="#3D7B19"/>
+      <g id="Kelopak">
+      <path id="Kelopak_kiri" d="M90.9436 48.4859C163.22 52.8358 231.581 93.5915 268.063 162.204C304.545 230.816 300.105 310.28 263.295 372.632C191.019 368.282 122.658 327.527 86.176 258.915C49.6942 190.303 54.1336 110.838 90.9436 48.4859Z" fill="#CE4848"/>
+      <path id="Kelopak_kanan" d="M426.415 48.3556C354.139 52.7055 285.777 93.4612 249.295 162.074C212.813 230.686 217.253 310.15 254.063 372.502C326.339 368.152 394.7 327.397 431.182 258.785C467.664 190.172 463.225 110.708 426.415 48.3556Z" fill="#E36464"/>
+      <path id="Kelopak_tengah" d="M259.119 5C320.893 42.7723 362.119 110.851 362.119 188.56C362.119 266.268 320.893 334.346 259.119 372.118C197.346 334.346 156.119 266.268 156.119 188.56C156.119 110.851 197.346 42.7723 259.119 5Z" fill="#C76767"/>
+      </g>
+      <path id="Daun_1" d="M342.825 434.558C346.054 454.918 338.644 477.426 321.306 493.594C303.968 509.762 280.999 515.583 260.914 510.942C257.685 490.582 265.094 468.075 282.432 451.907C299.771 435.739 322.74 429.917 342.825 434.558Z" fill="#0C4C07"/>
+      <path id="Daun_0" d="M170.606 556.011C168.09 576.471 176.28 598.706 194.172 614.259C212.064 629.813 235.222 634.828 255.133 629.489C257.649 609.029 249.46 586.794 231.568 571.241C213.676 555.688 190.517 550.671 170.606 556.011Z" fill="#0C4C07"/>
+      </g>
+      </svg>
+      <svg viewBox="0 0 518 796" className="w-[60%] z-2 h-auto absolute" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="Tulip">
+      <rect id="Tangkai" x="232.119" y="314" width="54" height="482" fill="#3D7B19"/>
+      <g id="Kelopak">
+      <path id="Kelopak_kiri" d="M90.9436 48.4859C163.22 52.8358 231.581 93.5915 268.063 162.204C304.545 230.816 300.105 310.28 263.295 372.632C191.019 368.282 122.658 327.527 86.176 258.915C49.6942 190.303 54.1336 110.838 90.9436 48.4859Z" fill="#CE4848"/>
+      <path id="Kelopak_kanan" d="M426.415 48.3556C354.139 52.7055 285.777 93.4612 249.295 162.074C212.813 230.686 217.253 310.15 254.063 372.502C326.339 368.152 394.7 327.397 431.182 258.785C467.664 190.172 463.225 110.708 426.415 48.3556Z" fill="#E36464"/>
+      <path id="Kelopak_tengah" d="M259.119 5C320.893 42.7723 362.119 110.851 362.119 188.56C362.119 266.268 320.893 334.346 259.119 372.118C197.346 334.346 156.119 266.268 156.119 188.56C156.119 110.851 197.346 42.7723 259.119 5Z" fill="#C76767"/>
+      </g>
+      <path id="Daun_1" d="M342.825 434.558C346.054 454.918 338.644 477.426 321.306 493.594C303.968 509.762 280.999 515.583 260.914 510.942C257.685 490.582 265.094 468.075 282.432 451.907C299.771 435.739 322.74 429.917 342.825 434.558Z" fill="#0C4C07"/>
+      <path id="Daun_0" d="M170.606 556.011C168.09 576.471 176.28 598.706 194.172 614.259C212.064 629.813 235.222 634.828 255.133 629.489C257.649 609.029 249.46 586.794 231.568 571.241C213.676 555.688 190.517 550.671 170.606 556.011Z" fill="#0C4C07"/>
+      </g>
+      </svg>
+      <svg viewBox="0 0 518 796" className="w-[35%] z-1 h-auto absolute right-[15%]" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="Tulip">
+      <rect id="Tangkai" x="232.119" y="314" width="54" height="482" fill="#3D7B19"/>
+      <g id="Kelopak">
+      <path id="Kelopak_kiri" d="M90.9436 48.4859C163.22 52.8358 231.581 93.5915 268.063 162.204C304.545 230.816 300.105 310.28 263.295 372.632C191.019 368.282 122.658 327.527 86.176 258.915C49.6942 190.303 54.1336 110.838 90.9436 48.4859Z" fill="#CE4848"/>
+      <path id="Kelopak_kanan" d="M426.415 48.3556C354.139 52.7055 285.777 93.4612 249.295 162.074C212.813 230.686 217.253 310.15 254.063 372.502C326.339 368.152 394.7 327.397 431.182 258.785C467.664 190.172 463.225 110.708 426.415 48.3556Z" fill="#E36464"/>
+      <path id="Kelopak_tengah" d="M259.119 5C320.893 42.7723 362.119 110.851 362.119 188.56C362.119 266.268 320.893 334.346 259.119 372.118C197.346 334.346 156.119 266.268 156.119 188.56C156.119 110.851 197.346 42.7723 259.119 5Z" fill="#C76767"/>
+      </g>
+      <path id="Daun_1" d="M342.825 434.558C346.054 454.918 338.644 477.426 321.306 493.594C303.968 509.762 280.999 515.583 260.914 510.942C257.685 490.582 265.094 468.075 282.432 451.907C299.771 435.739 322.74 429.917 342.825 434.558Z" fill="#0C4C07"/>
+      <path id="Daun_0" d="M170.606 556.011C168.09 576.471 176.28 598.706 194.172 614.259C212.064 629.813 235.222 634.828 255.133 629.489C257.649 609.029 249.46 586.794 231.568 571.241C213.676 555.688 190.517 550.671 170.606 556.011Z" fill="#0C4C07"/>
+      </g>
+      </svg>
+      <svg viewBox="0 0 518 796" className="w-[40%] z-0 h-auto absolute right-[-5%]" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="Tulip">
+      <rect id="Tangkai" x="232.119" y="314" width="54" height="482" fill="#3D7B19"/>
+      <g id="Kelopak">
+      <path id="Kelopak_kiri" d="M90.9436 48.4859C163.22 52.8358 231.581 93.5915 268.063 162.204C304.545 230.816 300.105 310.28 263.295 372.632C191.019 368.282 122.658 327.527 86.176 258.915C49.6942 190.303 54.1336 110.838 90.9436 48.4859Z" fill="#CE4848"/>
+      <path id="Kelopak_kanan" d="M426.415 48.3556C354.139 52.7055 285.777 93.4612 249.295 162.074C212.813 230.686 217.253 310.15 254.063 372.502C326.339 368.152 394.7 327.397 431.182 258.785C467.664 190.172 463.225 110.708 426.415 48.3556Z" fill="#E36464"/>
+      <path id="Kelopak_tengah" d="M259.119 5C320.893 42.7723 362.119 110.851 362.119 188.56C362.119 266.268 320.893 334.346 259.119 372.118C197.346 334.346 156.119 266.268 156.119 188.56C156.119 110.851 197.346 42.7723 259.119 5Z" fill="#C76767"/>
+      </g>
+      <path id="Daun_1" d="M342.825 434.558C346.054 454.918 338.644 477.426 321.306 493.594C303.968 509.762 280.999 515.583 260.914 510.942C257.685 490.582 265.094 468.075 282.432 451.907C299.771 435.739 322.74 429.917 342.825 434.558Z" fill="#0C4C07"/>
+      <path id="Daun_0" d="M170.606 556.011C168.09 576.471 176.28 598.706 194.172 614.259C212.064 629.813 235.222 634.828 255.133 629.489C257.649 609.029 249.46 586.794 231.568 571.241C213.676 555.688 190.517 550.671 170.606 556.011Z" fill="#0C4C07"/>
+      </g>
+      </svg>
+    </div>
+  </div>  
   )
 }
 
